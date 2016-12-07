c6d013e066311640cab66f72110ed0c467bc386b
Андрей Александров < >
2016-12-07 14:32:29 +0100

    Update gitignore

---
+++ src/consts.pas
@@ -0,0 +1,19 @@
+unit consts;
+
+interface
+
+uses SysUtils;
+
+function PATH_TO_SRC:string;
+
+implementation
+
+function PATH_TO_SRC: string;
+var App: string;
+begin
+  App:=ExtractFilePath(ParamStr(0));
+  Delete(App,Pos('\bin',App),Length('\bin'));
+  Result:=App;
+end;
+
+end.
