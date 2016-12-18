625f8936804c41852a619a13f98d3c0944921a98
Андрей Александров < >
2016-12-18 17:46:47 +0100

    Small work :)

--- src/bloglsresource.pas
+++ src/bloglsresource.pas
@@ -2,7 +2,7 @@ unit bloglsresource;
 
 interface
 
-uses djWebComponent, IdCustomHTTPServer;
+uses djWebComponent, IdCustomHTTPServer, Classes, SysUtils;
 
 type
     TBlogListResource = class(TdjWebComponent)
@@ -12,8 +12,16 @@ type
 implementation
 
     procedure TBlogListResource.OnGet(Request: TIdHTTPRequestInfo; Response: TIdHTTPResponseInfo);
+    var Header: TStringList;
+        Navbar: TStringList;
     begin
+      Header := TStringList.Create;
+      Navbar := TStringList.Create;
+      Header.LoadFromFile(PATH_TO_SRC + '/html/head.html');
+      Navbar.LoadFromFile(PATH_TO_SRC + 'html/navbar.html');
       
+      FreeAndNil(Header);
+      FreeAndNil(Navbar);
     end;
 
 end.
