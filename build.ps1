014a7677d6a17f1084d064de4491c4de2bbaafee
Андрей Александров < >
2016-12-18 18:53:41 +0100

    GetBlogs method and new dependency

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 cd .\src
 $out = fpc main.pas -Mdelphi -Fu".\dep\daraja-framework" `
    -Fu".\dep\indy\Core" -Fu".\dep\indy\Protocols" -Fu".\dep\indy\System" `
-   -Fu".\dep\dmustache" -FU".\out" -o".\bin\main.exe" | Out-String
+   -Fu".\dep\dmustache" -Fu".\dep\x-superobject" -FU".\out" -o".\bin\main.exe" | Out-String
 if ($out.Contains('Error')) {
     Write-Output "---FAIL----There are errors!!!------------------------"
     Write-Output $out
--- src/dao.pas
+++ src/dao.pas
@@ -2,9 +2,25 @@ unit dao;
 
 interface
 
-TDataAccessObject = class
-end;
+uses XSuperObject;
+
+type
+  TDataAccessObject = class
+    function GetBlogs: ISuperObject;
+  end;
 
 implementation
 
+function TDataAccessObject.GetBlogs: ISuperObject;
+var i: integer;
+begin
+  Result := SO;
+  for i:=0 to 0 do
+  with Result.A['posts'].O[i] do
+    begin
+      S['postname'] := 'Awesome blog post!';
+      S['preview'] := 'Awesome preview';
+    end;
+end;
+  
 end.
