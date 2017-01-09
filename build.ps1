2d475d433721975f354714cc31beff7ea21f183c
zawuza <andreizawuza@gmail.com>
2017-01-09 17:42:08 +0100

    Build script updated

--- build.ps1
+++ build.ps1
@@ -1,4 +1,10 @@
 Set-Location .\src
+if (!(Test-Path -Path ".\out")) {
+    New-Item -ItemType directory -Path ".\out" | Out-Null
+}
+if (!(Test-Path -Path ".\bin")) {
+    New-Item -ItemType directory -Path ".\bin" | Out-Null
+}
 $out = fpc main.pas -Mdelphi -Fu".\dep\daraja-framework" `
    -Fu".\dep\indy\Core" -Fu".\dep\indy\Protocols" -Fu".\dep\indy\System" `
    -Fu".\dep\dmustache" -FU".\out" -o".\bin\main.exe" | Out-String
--- src/blogarchiveresource.pas
+++ src/blogarchiveresource.pas
@@ -3,13 +3,23 @@ unit blogarchiveresource;
 
 interface
 
-uses djWebComponent, IdCustomHTTPServer;
+uses djWebComponent, IdCustomHTTPServer, consts, Classes, SysUtils;
 
 type
     TBlogArchiveResource = class(TdjWebComponent)
-      
+      procedure OnGet(Request: TIdHTTPRequestInfo; Response: TIdHTTPResponseInfo); override;
     end;
 
 implementation
+
+procedure TBlogArchiveResource.OnGet(Request: TIdHTTPRequestInfo; Response: TIdHTTPResponseInfo);
+var Navbar: TStringList;
+    Header: TStringList;
+begin
+   Navbar := TStringList.Create;
+   Header := TSTringList.Create;
+   FreeAndNil(Navbar);
+   FreeAndNil(Header);
+end;
 
 end.
