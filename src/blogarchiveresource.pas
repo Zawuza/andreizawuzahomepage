9fcd315ce81ff0b252cd1c71936e5abb475bf3f5
zawuza <andreizawuza@gmail.com>
2017-01-10 21:59:40 +0100

    Blog archive improved

--- src/blogarchiveresource.pas
+++ src/blogarchiveresource.pas
@@ -12,6 +12,15 @@ type
       procedure OnGet(Request: TIdHTTPRequestInfo; Response: TIdHTTPResponseInfo); override;
     end;
 
+
+    TBlogPostText = record
+      Rus: string;
+      Ger: string;
+      Eng: string;
+    end;
+
+    function GetBlogById(Id:string): TBlogPostText;
+
 implementation
 
 procedure TBlogArchiveResource.OnGet(Request: TIdHTTPRequestInfo; Response: TIdHTTPResponseInfo);
@@ -20,12 +29,13 @@ var Navbar: TStringList;
     buf: TStringList;
     mustache: TSynMustache;
     doc: Variant;
+    BlogPost: TBlogPostText;
 begin
    {Create resources}
    Navbar := TStringList.Create;
    Header := TStringList.Create;
    buf:=TStringList.Create;
-   {Prepare files}
+   {Prepare html parts}
    Navbar.LoadFromFile(PATH_TO_SRC + '\html\navbar.html');
    Header.LoadFromFile(PATH_TO_SRC + '\html\head.html');
    buf.LoadFromFile(PATH_TO_SRC + '\html\archive.html');
@@ -34,6 +44,11 @@ begin
    TDocVariant.new(doc);
    doc.head:=Header.Text;
    doc.navbar:=Navbar.Text;
+   {Get blog content and add to html}
+   BlogPost:=GetBlogById('NOT IMPLEMENTED');
+   doc.russian:=BlogPost.Rus;
+   doc.german:=BlogPost.Ger;
+   doc.english:=BlogPost.Eng;
    {Return html}
    Response.ContentText:=mustache.Render(doc);
    Response.ContentType:='text/html';
@@ -43,6 +58,14 @@ begin
    FreeAndNil(Navbar);
    FreeAndNil(Header);
    FreeAndNil(buf);
+end;
+
+
+function GetBlogById(Id: string): TBlogPostText;
+begin
+  Result.Rus:='<h1>Привет</h1>';
+  Result.Ger:='<h1>Hallo</h1>';
+  Result.Eng:='<h1>Hello</h1>';
 end;
 
 end.
