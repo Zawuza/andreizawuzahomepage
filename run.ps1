9ddfbafceb5c86d3ad1e13c0c6560e8befc5d586
zawuza <andreizawuza@gmail.com>
2017-02-08 11:35:40 +0100

    Update run script

--- run.ps1
+++ run.ps1
@@ -1,1 +1,9 @@
-.\src\bin\main.exe | Out-File .\log.log
+try 
+{
+Get-Process main | Stop-Process 
+} 
+catch 
+{
+  Write-Output "main not found"
+}
+Start-Process .\src\bin\main.exe | Out-File .\log.log
